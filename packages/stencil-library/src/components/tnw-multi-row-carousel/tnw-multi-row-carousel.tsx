8c414cb2d9050328073e1ebfe783a16d06344200
Khaled Alshibani <alshibanikhaleds@gmail.com>
2024-12-08 10:56:34 +0300

    fix(tnw-multi-row-carousel): resolve slot element assignment issues for React
    - Refactored slot assignment functionality in `tnw-multi-row-carousel` to introduce a
    structured slot naming convention (`row-[n]`) instead of using the default slot.
    - Improved slot handling ensures compatibility and proper rendering of child elements in React.
    - Updated the `tnw-multi-row-carousel` API documentation.

--- packages/stencil-library/src/components/tnw-multi-row-carousel/tnw-multi-row-carousel.tsx
+++ packages/stencil-library/src/components/tnw-multi-row-carousel/tnw-multi-row-carousel.tsx
@@ -1,14 +1,13 @@
-import { Component, Host, Prop, Element, State, h } from '@stencil/core';
+import { Component, Host, Prop, Element, h } from '@stencil/core';
 import { GLOBAL_PREFIX, isAdoptedStyleSheetsSupported, isCSSStyleSheetSupported } from '../../utils/utils';
-import { styles } from './tnw-multi-row-carousel.style';
-import { borderRadiusStyleSheet, extendedAppearanceStyleSheet } from '../../utils/shared-styles';
 import { validateProps } from './utils/tnw-multi-row-carousel-validate-props';
+import { styles } from './tnw-multi-row-carousel.style';
 
 /**
- * The `tnw-multi-row-carousel` provides an animated, infinitely scrolling carousel 
+ * The `tnw-multi-row-carousel` component provides an animated, infinitely scrolling carousel 
  * with multiple rows. Each row scrolls independently and can move in alternating directions.
  *
- * @slot - Default slot to provide the content for the carousel. Each row will auto-distribute the slot's content.
+ * @slot row-[number] - A slot for each row's content. Replace `[number]` with the row index (starting from 1). Ensure the number of slots matches the `rows` prop.
  */
 @Component({
   tag: 'tnw-multi-row-carousel',
@@ -17,8 +16,6 @@ import { validateProps } from './utils/tnw-multi-row-carousel-validate-props';
 export class TnwMultiRowCarousel {
   private baseClass = `${GLOBAL_PREFIX}-multi-row-carousel`;
   private componentStyles: CSSStyleSheet;
-
-  @State() rowContents: HTMLElement[][] = [];
 
   @Element() el!: HTMLTnwMultiRowCarouselElement;
 
@@ -44,8 +41,6 @@ export class TnwMultiRowCarousel {
   connectedCallback(): void {
     if (isAdoptedStyleSheetsSupported()) {
       (this.el.shadowRoot as any).adoptedStyleSheets = [
-        extendedAppearanceStyleSheet,
-        borderRadiusStyleSheet,
         this.componentStyles
       ];
     } else {
@@ -60,25 +55,18 @@ export class TnwMultiRowCarousel {
     validateProps(propsValues);
   }
 
-  componentDidLoad() {
-    const slotContent = Array.from(this.getSlotContent());
-    this.rowContents = this.distributeContentIntoRows(slotContent, this.rows);
+  private handleRowHover(rowElement: HTMLElement, isHover: boolean): void {
+    rowElement.style.animationPlayState = isHover ? 'paused' : 'running';
   }
 
-  private getSlotContent(): HTMLElement[] {
-    const fallbackContent = Array.from(this.el.children) as HTMLElement[];
-    fallbackContent.forEach(child => child.remove());
-
-    return fallbackContent;
+  private handleMouseEnter(event: MouseEvent): void {
+    const rowElement = event.currentTarget as HTMLElement;
+    this.handleRowHover(rowElement, true);
   }
 
-  private distributeContentIntoRows(content: HTMLElement[], rows: number): HTMLElement[][] {
-    const distributed: HTMLElement[][] = Array.from({ length: rows }, () => []);
-    content.forEach((item, index) => {
-      const rowIndex = index % rows;
-      distributed[rowIndex].push(item.cloneNode(true) as HTMLElement);
-    });
-    return distributed;
+  private handleMouseLeave(event: MouseEvent): void {
+    const rowElement = event.currentTarget as HTMLElement;
+    this.handleRowHover(rowElement, false);
   }
 
   private getRowStyles(rowIndex: number): { [key: string]: string } {
@@ -96,38 +84,18 @@ export class TnwMultiRowCarousel {
     };
   }
 
-  private handleRowHover(rowElement: HTMLElement, isHover: boolean): void {
-    rowElement.style.animationPlayState = isHover ? 'paused' : 'running';
-  }
-
-  private handleMouseEnter(event: MouseEvent): void {
-    const rowElement = event.currentTarget as HTMLElement;
-    this.handleRowHover(rowElement, true);
-  }
-
-  private handleMouseLeave(event: MouseEvent): void {
-    const rowElement = event.currentTarget as HTMLElement;
-    this.handleRowHover(rowElement, false);
-  }
-
   render() {
     return (
       <Host class={this.baseClass}>
         <div class={`${this.baseClass}__container`}>
-          {this.rowContents.map((row, rowIndex) => (
+          {Array.from({ length: this.rows }, (_, index) => (
             <div
               class={`${this.baseClass}__row`}
-              style={this.getRowStyles(rowIndex)}
+              style={this.getRowStyles(index)}
               onMouseEnter={this.handleMouseEnter}
               onMouseLeave={this.handleMouseLeave}
             >
-              {row.map((item, itemIndex) => (
-                <div
-                  class={`${this.baseClass}__item`}
-                  key={`${rowIndex}-${itemIndex}`}
-                  innerHTML={item.outerHTML}
-                ></div>
-              ))}
+              <slot name={`row-${index + 1}`} />
             </div>
           ))}
         </div>

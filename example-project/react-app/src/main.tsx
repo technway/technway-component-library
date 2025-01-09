cd9e1ccd8d291ccb7c449b0a610cec8a428b4fd5
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-09 20:21:56 +0300

    chore: organize react-app project example

--- example-project/react-app/src/main.tsx
+++ example-project/react-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '../../../node_modules//@technway/stencil-library/dist/stencil-library/stencil-library.css'
+import '../../../node_modules/@technway/layout-kit/dist/css/grid.css';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(

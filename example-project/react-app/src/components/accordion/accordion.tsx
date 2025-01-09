cc184b3a789c02ed4f6b44f5ecdf8b52c056ad7e
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-09 20:21:56 +0300

    chore: organize react-app project example

---
+++ example-project/react-app/src/components/accordion/accordion.tsx
@@ -0,0 +1,18 @@
+import {
+    TnwAccordion
+} from '@technway/react-library/src/components';
+
+interface AccordionProps {
+    expand: boolean;
+}
+const Accordion = (props: AccordionProps) => {
+    return (
+        <TnwAccordion
+            heading="Accordion Test 1"
+            content="This is the content of the first accordion item."
+            expand={props.expand}
+        />
+    )
+}
+
+export default Accordion;

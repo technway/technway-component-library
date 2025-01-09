fe546cbe619a058962d9384d61b74ce805931202
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-09 16:38:02 +0300

    chore: add react app project example for testing

---
+++ example-project/react-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { TnwAccordion } from '@technway/react-library/src/components'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <TnwAccordion
+          heading='Accordion Heading'
+          content='Some content'
+        />
+      </div>
+    </>
+  )
+}
+
+export default App

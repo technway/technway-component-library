6991c97545e0420cb35571b3f8ce624280c1194c
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-13 12:25:56 +0300

    chore: clean node_modules

--- packages/stencil-library/components/index.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Built with Stencil
- * Copyright (c) Tecchnway.biz.
- */
-export { g as getAssetPath, s as setAssetPath, a as setNonce, b as setPlatformOptions } from './p-9d664f2a.js';
-
-//# sourceMappingURL=index.js.map

d956a4d631cc4599e5cd31c784e1ad22757d77fe
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-13 21:09:36 +0300

    fix(next-library, layout-kit): update and add @types/react to solve typings errors
    - Added `@types/react` as a development dependency in **next-library**.
    - Upgraded `@types/react` from version 18.x to 19.x in **layout-kit** for
    compatibility with the latest React features.
    - Resolved client-side rendering (CSR) issues in the **Next.js v15 app**,
    verified through testing in `example-project/next-v15-app`.

---
+++ example-project/next-v15-app/src/components/accordion/accordion-group.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import {
+    TnwAccordion,
+    TnwAccordionGroup
+} from '@technway/next-library/src/components';
+
+const AccordionGroup = () => {
+    return (
+        <TnwAccordionGroup
+            singleExpand={true}
+        >
+            <TnwAccordion
+                heading="Accordion Item 1"
+                content="This is the content for the first item."
+                appearance="underlined">
+            </TnwAccordion>
+            <TnwAccordion
+                heading="Accordion Item 2"
+                content="This is the content for the second item."
+                appearance="underlined">
+            </TnwAccordion>
+            <TnwAccordion
+                heading="Accordion Item 3"
+                content="This is the content for the third item."
+                appearance="underlined">
+            </TnwAccordion>
+        </TnwAccordionGroup>
+    )
+}
+
+export default AccordionGroup;

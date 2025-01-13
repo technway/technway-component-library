67a9519a075e66feacc407ba0de087df57f281be
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-13 21:09:36 +0300

    fix(next-library, layout-kit): update and add @types/react to solve typings errors
    - Added `@types/react` as a development dependency in **next-library**.
    - Upgraded `@types/react` from version 18.x to 19.x in **layout-kit** for
    compatibility with the latest React features.
    - Resolved client-side rendering (CSR) issues in the **Next.js v15 app**,
    verified through testing in `example-project/next-v15-app`.

---
+++ example-project/next-v15-app/src/components/card/card.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { TnwCard } from '@technway/next-library/src/components';
+
+type ArticleCardProps = {
+    layout?: 'vertical' | 'horizontal';
+    appearance?: "transparent" | "none" | "solid" | "outlined" | "mixed";
+    appearanceColor?: "primary" | "secondary" | "auto" | "inverse" | "light" | "white" | "black";
+    padding?: "sm" | "md" | "lg" | "none";
+    imageHeight?: string;
+}
+
+const ArticleCard = (props: ArticleCardProps) => {
+    const randomHeight = Math.floor(Math.random() * (300 - 250 + 1)) + 250;
+    const imageSrc = `https://picsum.photos/512/${randomHeight}`;
+    const heading = "Agile Project Management: Tips for Effective Team Collaboration";
+    const description = "Agile project management has become increasingly popular in recent years as more and more organizations recognize the value of a flexible, collaborative approach to project management. In this article, we will provide some tips for achieving effective team collaboration in an agile project management context.";
+    const link = "#";
+    const date = "March 2023";
+    const category = "Project Management";
+
+    return (
+        <TnwCard
+            badgeLabel={category}
+            buttonLabel="Read more"
+            buttonHref={link}
+            buttonRadius="full"
+            contentSpacing="md"
+            date={date}
+            description={description}
+            heading={heading}
+            imageSrc={imageSrc}
+            imageAlt={heading}
+            layout={props.layout || 'vertical'}
+            appearance={props.appearance || 'none'}
+            appearanceColor={props.appearanceColor || 'auto'}
+            padding={props.padding || 'none'}
+            spacing="md"
+            imageHeight={props.imageHeight}
+        />
+    );
+};
+
+export default ArticleCard;

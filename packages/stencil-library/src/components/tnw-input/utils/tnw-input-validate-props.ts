d02e5cf3eb91f12f3d689596639725a645755f4a
Khaled Alshibani <127689031+khaledsAlshibani@users.noreply.github.com>
2025-01-25 16:05:48 +0300

    refactor(tnw-input): change required props to optional

--- packages/stencil-library/src/components/tnw-input/utils/tnw-input-validate-props.ts
+++ packages/stencil-library/src/components/tnw-input/utils/tnw-input-validate-props.ts
@@ -144,7 +144,7 @@ export function validateProps(propsValues: any[]): void {
     "type": [
       "string"
     ],
-    "isRequired": true
+    "isRequired": false
   },
   {
     "name": "sanitizeInput",
@@ -169,7 +169,7 @@ export function validateProps(propsValues: any[]): void {
     "type": [
       "string"
     ],
-    "isRequired": true
+    "isRequired": false
   },
   {
     "name": "value",
